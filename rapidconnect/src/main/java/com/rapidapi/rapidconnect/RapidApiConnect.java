23105fc972dac7f61dd11a571929250df4dd8c85
Spyes <lewis@rapidapi.com>
2017-03-07 14:21:08 +0200

    webhook fixes

--- rapidconnect/src/main/java/com/rapidapi/rapidconnect/RapidApiConnect.java
+++ rapidconnect/src/main/java/com/rapidapi/rapidconnect/RapidApiConnect.java
@@ -1,8 +1,7 @@
 package com.rapidapi.rapidconnect;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -24,109 +23,110 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class RapidApiConnect {
-  private static final RequestBody EMPTY_REQUEST_BODY = RequestBody.create(MediaType.parse("text/plain"), "");
-  private final OkHttpClient client;
-  private final String project;
-  private final String key;
+    private static final RequestBody EMPTY_REQUEST_BODY = RequestBody.create(MediaType.parse("text/plain"), "");
+    private final OkHttpClient client;
+    private final String project;
+    private final String key;
 
     public interface WebhookEvents {
-        void onMessage(JsonElement msg);
+        void onMessage(JsonNode msg);
         void onClose(int code, String reason);
-        void onError(Exception e);
+        void onError(String reason);
+        void onJoin();
     };
-    
-  public RapidApiConnect(String project, String key) {
-    this.client = new OkHttpClient();
-    this.project = project;
-    this.key = key;
-  }
 
-  /**
-  * Returns the base URL for block calls
-  *
-  * @return string Base URL for block calls
-  */
-  private static String getBaseUrl()
-  {
-    return "https://rapidapi.io/connect";
-  }
+    public RapidApiConnect(String project, String key) {
+        this.client = new OkHttpClient();
+        this.project = project;
+        this.key = key;
+    }
 
-  /**
-  * Build a URL for a block call
-  *
-  * @param pack Package where the block is
-  * @param block Block to be called
-  * @return string Generated URL
-  */
-  public static String blockUrlBuild(String pack, String block)
-  {
-    return RapidApiConnect.getBaseUrl() + "/" + pack + "/" + block;
-  }
+    /**
+     * Returns the base URL for block calls
+     *
+     * @return string Base URL for block calls
+     */
+    private static String getBaseUrl()
+    {
+        return "https://rapidapi.io/connect";
+    }
+
+    /**
+     * Build a URL for a block call
+     *
+     * @param pack Package where the block is
+     * @param block Block to be called
+     * @return string Generated URL
+     */
+    public static String blockUrlBuild(String pack, String block)
+    {
+        return RapidApiConnect.getBaseUrl() + "/" + pack + "/" + block;
+    }
 
     private static String callbackBaseUrl() { return "https://webhooks.rapidapi.xyz"; }
 
     private static String websocketBaseUrl() { return "wss://webhooks.rapidapi.xyz"; }
 
-  /**
-  * Call a block
-  *
-  * @param pack Package of the block
-  * @param block Name of the block
-  * @param body Arguments to send to the block (Map)
-  * @return Map
-  */
-  public Map<String, Object> call(String pack, String block, Map<String, Argument> body) throws IOException {
-    Map<String, Object> result = new HashMap<>();
+    /**
+     * Call a block
+     *
+     * @param pack Package of the block
+     * @param block Name of the block
+     * @param body Arguments to send to the block (Map)
+     * @return Map
+     */
+    public Map<String, Object> call(String pack, String block, Map<String, Argument> body) throws IOException {
+        Map<String, Object> result = new HashMap<>();
 
-    RequestBody requestBody;
-    if (body == null || body.isEmpty()) {
-      requestBody = EMPTY_REQUEST_BODY;
-    } else {
-      MultipartBody.Builder builder = new MultipartBody.Builder()
-              .setType(MultipartBody.FORM);
-
-      Set<Map.Entry<String, Argument>> entrySet = body.entrySet();
-      for (Map.Entry<String, Argument> entry : entrySet) {
-        Argument argument = entry.getValue();
-        if ("data".equals(argument.getType())) {
-          builder.addFormDataPart(entry.getKey(), argument.getValue());
+        RequestBody requestBody;
+        if (body == null || body.isEmpty()) {
+            requestBody = EMPTY_REQUEST_BODY;
         } else {
-          File file = new File(argument.getValue());
-          if (file.exists() && file.isFile()) {
-            builder.addFormDataPart(entry.getKey(), file.getName(), RequestBody.create(MultipartBody.FORM, file));
-          } else {
-            result.put("error", "File not exist or can't be read.");
+            MultipartBody.Builder builder = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM);
 
-            return result;
-          }
+            Set<Map.Entry<String, Argument>> entrySet = body.entrySet();
+            for (Map.Entry<String, Argument> entry : entrySet) {
+                Argument argument = entry.getValue();
+                if ("data".equals(argument.getType())) {
+                    builder.addFormDataPart(entry.getKey(), argument.getValue());
+                } else {
+                    File file = new File(argument.getValue());
+                    if (file.exists() && file.isFile()) {
+                        builder.addFormDataPart(entry.getKey(), file.getName(), RequestBody.create(MultipartBody.FORM, file));
+                    } else {
+                        result.put("error", "File not exist or can't be read.");
+
+                        return result;
+                    }
+                }
+            }
+            requestBody = builder.build();
         }
-      }
-      requestBody = builder.build();
+
+        Request request = new Request.Builder()
+                .url(RapidApiConnect.blockUrlBuild(pack, block))
+                .addHeader("User-Agent", "RapidAPIConnect_Java")
+                .addHeader("Authorization", Credentials.basic(this.project, this.key))
+                .post(requestBody)
+                .build();
+
+        try (Response response = this.client.newCall(request).execute()) {
+            Gson gson = new Gson();
+
+            Map<String, Object> map = gson.fromJson(response.body().string(), new TypeToken<Map<String, Object>>(){}.getType());
+
+            if(response.code() != 200 || "error".equals(map.get("outcome"))){
+                result.put("error", map.get("payload"));
+
+                return result;
+            }else{
+                result.put("success", map.get("payload"));
+
+                return result;
+            }
+        }
     }
-
-    Request request = new Request.Builder()
-      .url(RapidApiConnect.blockUrlBuild(pack, block))
-      .addHeader("User-Agent", "RapidAPIConnect_Java")
-      .addHeader("Authorization", Credentials.basic(this.project, this.key))
-      .post(requestBody)
-      .build();
-
-    try (Response response = this.client.newCall(request).execute()) {
-      Gson gson = new Gson();
-
-      Map<String, Object> map = gson.fromJson(response.body().string(), new TypeToken<Map<String, Object>>(){}.getType());
-
-      if(response.code() != 200 || "error".equals(map.get("outcome"))){
-        result.put("error", map.get("payload"));
-
-        return result;
-      }else{
-        result.put("success", map.get("payload"));
-
-        return result;
-      }
-    }
-  }
 
     public Map listen(String pack, String block, Map<String, String> parameters, final WebhookEvents callbacks) throws IOException {
         Map<String, Object>result =  new HashMap<>();
@@ -144,26 +144,28 @@ public class RapidApiConnect {
             final Map<String, Object> map = gson.fromJson(response.body().string(), new TypeToken<Map<String, Object>>(){}.getType());
             try {
                 String socket_url = RapidApiConnect.websocketBaseUrl() + "/socket/websocket?token=" + map.get("token");
+                final String token = (String)map.get("token");
                 Socket socket;
                 Channel channel;
 
                 socket = new Socket(socket_url);
                 socket.connect();
-                ObjectNode node = new ObjectNode(JsonNodeFactory.instance)
-                        .put("token", "ydt3vFyVEoW51ZFCC2i5QKab");
-                channel = socket.chan("users_socket:" + userId, node);
+                ObjectMapper mapper = new ObjectMapper();
+                channel = socket.chan("users_socket:" + userId, mapper.convertValue(parameters, JsonNode.class));
                 channel.join()
                         .receive("ok", new IMessageCallback() {
                             @Override
                             public void onMessage(Envelope envelope) {
-                                System.out.println(envelope.toString());
+                                callbacks.onJoin();
                             }
                         });
 
                 channel.on("new_msg", new IMessageCallback() {
                     @Override
                     public void onMessage(Envelope envelope) {
-                        if (map.get("token") == envelope.getPayload().get("token")) {
+                        if (envelope.getPayload().get("token") == null) {
+                            callbacks.onError("Token error");
+                        } else if (token.equals(envelope.getPayload().get("token").asText())) {
                             callbacks.onMessage(envelope.getPayload().get("body"));
                         }
                     }
